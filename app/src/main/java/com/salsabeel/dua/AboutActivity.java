3b952384e6d497c5c1f30fb80c16bc2417cc32d2
sulaiman <mohamedsulaiman2020@gmail.com>
2017-09-16 23:10:49 +0530

    add about page and social share links

--- app/src/main/java/com/salsabeel/dua/AboutActivity.java
+++ app/src/main/java/com/salsabeel/dua/AboutActivity.java
@@ -1,22 +1,34 @@
 package com.salsabeel.dua;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.salsabeel.dua.utils.ContextWrapper;
 
 import java.util.Locale;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "AboutActivity";
 
     SharedPreferences sharedPreferences;
     private String currentLang;
+
+    private ImageView mFacebook;
+    private ImageView mTwitter;
+    private ImageView mYoutube;
+    private Intent intent;
+    private TextView mCopyRight;
 
 
     @Override
@@ -25,10 +37,21 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         setTitle(getResources().getString(R.string.about));
 
+        mCopyRight = (TextView) findViewById(R.id.txt_copy_name);
+        mFacebook = (ImageView) findViewById(R.id.image_facebook);
+        mTwitter = (ImageView) findViewById(R.id.image_twitter);
+        mYoutube = (ImageView)findViewById(R.id.image_youtube);
+
+        mCopyRight.setOnClickListener(this);
+        mFacebook.setOnClickListener(this);
+        mTwitter.setOnClickListener(this);
+        mYoutube.setOnClickListener(this);
 
         sharedPreferences  = PreferenceManager.getDefaultSharedPreferences(this);
         currentLang = sharedPreferences.getString("pref_lang_key","ta");
     }
+
+
 
     @Override
     public void onResume(){
@@ -52,5 +75,31 @@ public class AboutActivity extends AppCompatActivity {
         newLocale = new Locale(langPref);
         Context context = ContextWrapper.wrap(newBase, newLocale);
         super.attachBaseContext(context);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.image_facebook:
+                this.intent = new Intent("android.intent.action.VIEW");
+                this.intent.setData(Uri.parse("https://www.facebook.com/Darul-Huda-Publication-134202593299656/"));
+                startActivity(this.intent);
+                break;
+            case R.id.image_twitter:
+                this.intent = new Intent("android.intent.action.VIEW");
+                this.intent.setData(Uri.parse("https://twitter.com/darulhudaonline?lang=en"));
+                startActivity(this.intent);
+                break;
+            case R.id.image_youtube:
+                this.intent = new Intent("android.intent.action.VIEW");
+                this.intent.setData(Uri.parse("https://www.youtube.com/darulhudanet"));
+                startActivity(this.intent);
+                break;
+            case R.id.txt_copy_name:
+                this.intent = new Intent("android.intent.action.VIEW");
+                this.intent.setData(Uri.parse("http://www.darulhuda.net/"));
+                startActivity(this.intent);
+                break;
+            default:
+        }
     }
 }
